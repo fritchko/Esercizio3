14d4e6ccc54b8e2ee2df2fa764f65c4b7e771760
Davide Francesco Riccio <ledrazors@gmail.com>
2023-06-08 03:36:35 +0200

    Initial commit

---
+++ src/co/develhope/introduction/PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+package co.develhope.introduction;
+
+public class PrintYourNameAndSurname {
+    public static void main(String[] args) {
+        System.out.print("Davide ");
+        System.out.print("Riccio");
+    }
+}
